40b3703bf8f89443ea09d5c8b5ad69369616321d
PedroMorenoTrujillo <43439808+PedroMorenoTrujillo@users.noreply.github.com>
2019-12-12 20:02:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require("mongoose");
 var app = require("./app");
-var port = 3900;
+var port = 5000;
 
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
